ebca8da261ac805402397d55b136f48e7f9b8e5a
Sloan Austermann <sloanaustermann@Sloans-MBP.home>
2021-01-03 19:00:31 -0500

    image compression

---
+++ app/main.py
@@ -0,0 +1,79 @@
+# export FLASK_APP=app/main.py
+#  export FLASK_ENV=development
+# flask run
+
+from flask import Flask, request,jsonify, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+from PIL import Image
+import io
+import os
+from app.utils import face_detector, dog_detector, get_breeds, predict_breed, reset
+
+app = Flask(__name__, static_url_path='/static')
+import os
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+#face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
+
+def allowed_file(filename):
+    # xxx.png
+    
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def file_size(filename):
+    os.stat('somefile.txt').st_size
+
+#GET IMAGE AS DIRECT INPUT INTO FILES
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+   
+    
+
+
+@app.route('/classify', methods=['POST'])
+def classify():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file is None or file.filename == "":
+            out = 'no file'
+            return render_template('index.html', prediction_text=out)
+        if not allowed_file(file.filename):
+            out = 'format not supported.'
+            return render_template('index.html', prediction_text=out)
+        
+        #try:
+
+        img = file.read()
+        img_face = Image.open(io.BytesIO(img))
+        img_dog = Image.open(io.BytesIO(img)).convert('RGB')
+        img_save = img_dog.save("app/images/image.jpg",'JPEG')
+        file_size = os.path.getsize("app/images/image.jpg")
+
+        if file_size > 1000000:
+            img_dog.save("app/images/image.jpg",'JPEG',optimize = True,  
+                 quality = 10) 
+
+        img_dog = Image.open("app/images/image.jpg")
+        faces = face_detector(img_face)
+        dogs = dog_detector(img_dog)
+
+        breeds = list(reversed(predict_breed(img_dog))) #.split(',')[0][1:-1]
+        breeds = [breed.split(',')[0][1:-1] for breed in breeds]
+        
+        if faces:
+            out = f"Hello Hooman! You look a lot like a {breeds[0]}!"
+        elif dogs:
+            out = f"This doggo looks like a {breeds[0]}!"
+        else:
+            out = f"Hmm, I dont see a Hooman or Doggo in this picture. Try another one!"
+
+        #return_dict = {'file_name': str(file.filename), 'out':out}
+        reset()
+        img = None
+        file = None
+        faces = None
+        dogs = None
+        breeds = None
+        return render_template('index.html', prediction_text=out)
+        #    return jsonify({'error': 'error during prediction'})
---
+++ app/utils.py
@@ -0,0 +1,108 @@
+import io
+import os
+import torch
+from PIL import Image
+import torchvision.transforms as transforms
+import torchvision.models as models
+import torch.nn as nn
+import cv2
+import numpy as np
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+
+
+face_cascade = cv2.CascadeClassifier('app/opencv-front-face-default.xml')
+face_cascade2 = cv2.CascadeClassifier('app/haarcascade_frontalface_alt.xml')
+
+def reset():
+    face_cascade = None
+    face_cascade2 = None
+    BREEDS = None
+    NUM_BREEDS = None
+    model = None
+    img = None
+    img_t = None
+    batch_t = None
+    out = None
+    breeds = None
+
+
+transform = transforms.Compose([            
+    transforms.Resize(256),                    
+    transforms.CenterCrop(224),               
+    transforms.ToTensor(),                   
+    transforms.Normalize(                      
+    mean=[0.485, 0.456, 0.406],               
+    std=[0.229, 0.224, 0.225]                  
+          )])
+
+
+
+def get_breeds():
+    file = open("app/breeds.txt")
+    breeds = list(file)
+    file.close()
+    return breeds
+
+BREEDS = get_breeds()
+NUM_BREEDS = len(get_breeds())
+
+
+def face_detector(img):
+    gray = cv2.cvtColor(np.float32(img), cv2.COLOR_BGR2GRAY)
+    gray = np.array(gray, dtype='uint8')
+    faces = face_cascade.detectMultiScale(gray)
+    faces2 = face_cascade2.detectMultiScale(gray)
+    return min(int(len(faces)), int(len(faces2))) > 0 
+
+def dog_detector(img):
+    ## : Complete the function.
+
+    '''
+    Use pre-trained VGG-16 model to obtain index corresponding to 
+    predicted ImageNet class for image at specified path
+    Args:
+        img_path: path to an image  
+    Returns:
+        Index corresponding to VGG-16 model's prediction
+    '''
+    squeezenet = models.squeezenet1_1(pretrained=True)
+    squeezenet.eval()
+    transform = transforms.Compose([            
+    transforms.Resize(256),                    
+    transforms.CenterCrop(224),               
+    transforms.ToTensor(),                   
+    transforms.Normalize(                      
+    mean=[0.485, 0.456, 0.406],               
+    std=[0.229, 0.224, 0.225]                  
+          )])
+    
+    img_t = transform(img)
+    batch_t = torch.unsqueeze(img_t, 0)
+    out = squeezenet(batch_t)
+    out = torch.nn.functional.softmax(out, dim=1)[0] * 100
+    return 150 < int(out.argmax()) < 269 # true/false
+
+def predict_breed(img):
+    # load the image and return the predicted breed
+    
+    model = models.densenet121(pretrained=True)
+ 
+    model.classifier=nn.Sequential(nn.Linear(1024,512),
+                                        nn.ReLU(),
+                                        nn.Dropout(0.2),
+                                       nn.Linear(512,133))
+
+
+    PATH = "app/model.pth"
+    model.load_state_dict(torch.load(PATH, map_location=torch.device('cpu') ))
+    model.eval()
+    img_t = transform(img)
+    batch_t = torch.unsqueeze(img_t, 0)
+    out = model(batch_t)
+    probs = torch.nn.functional.softmax(out, dim=1)[0] * 100
+    args = probs.argsort()[-3:]
+    
+    return list(BREEDS[int(arg)] for arg in args)
+    #return out
